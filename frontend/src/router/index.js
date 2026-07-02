aba2ae7fbc5234dc7edb65391eb9fecabdd6c305
여인범 <inbeom1202@gmail.com>
2026-07-02 11:24:56 +0900

    feat: frontend 초기 세팅

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// 기본 라우팅
+import { createRouter, createWebHistory } from "vue-router";
+import Homepage from "@/pages/Homepage.vue";
+import authRoutes from "./auth";
+import boardRoutes from "./board";
+import travelRoutes from "./travel";
+import galleryRoutes from "./gallery";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    { path: "/", name: "home", component: Homepage },
+    ...authRoutes,
+    ...boardRoutes,
+    ...travelRoutes,
+    ...galleryRoutes,
   ],
-})
+});
 
-export default router
+export default router;
